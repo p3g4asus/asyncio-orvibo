4625939311c80d46eea3b819b70dc1aa94f2db12
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2019-05-01 17:20:19 +0200

    Version 1.17: Added subscribe_test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="asyncio-orvibo",
-    version="1.16",
+    version="1.17",
     author="p3g4asus",
     author_email="fulminedipegasus@gmail.com",
     description="Asyncio module for Orvibo devices control",
